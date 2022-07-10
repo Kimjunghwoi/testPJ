567c805fc349b1ac38c8fb3df16f7f3cf11c7670
kjh <poiuy5947@gmail.com>
2022-07-11 00:30:46 +0900

    test

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.discount.Discountpolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    private final Discountpolicy discountpolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, Discountpolicy discountpolicy) {
+        this.memberRepository = memberRepository;
+        this.discountpolicy = discountpolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountpolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+
+    }
+}
